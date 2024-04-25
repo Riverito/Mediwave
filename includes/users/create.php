3fab31b772a869070d313a360af1379c2724beda
StefzSA <stefano.g.strippoli@gmail.com>
2024-04-24 21:37:29 -0400

    More changes to the create.php and functions related to it.

--- includes/users/create.php
+++ includes/users/create.php
@@ -9,32 +9,34 @@ $errorEmpty = false;
 $errorEmail = false;
 
 if (!empty($_POST)) {
-    //Utiliza condicionales de guardia mamaguebador xd
-    //Utiliza sólo ifs, si es true la condicion entonces cambia el status.
-    //Si después de cambiar el código de status también necesitas un mensaje custom, mándalo desde acá, no desde el js.
-    //Acuerdate que el código procedural va de arriba a abajo, intenta hacer un proceso en el cual las condiciones de guardia
-    //detengan el proceso si se llegan a cumplir. Sólo con ifs, no es necesario el else if.
-    
     if (emptyInputSignup( $_POST["user_name"], $_POST["pwd"], $_POST["pwdrepeat"], $_POST["user_apellido"], $_POST["userCd"], $_POST["userEmail"], $_POST["userRol"])) {
         $response['status'] = 1;
+        reportKill($response);
     }
 
     if (invalidName($_POST["user_name"]) or invalidName($_POST["user_apellido"])) {
         $response['status'] = 2;
+        reportKill($response);
     }
 
     if (invalidCd($_POST["userCd"])){
         $response['status'] = 3;
+        reportKill($response);
     }
 
     if (pwdMatch($_POST["pwd"], $_POST["pwdrepeat"])){
         $response['status'] = 4;
+        reportKill($response);
     }
     if(!valid_email($_POST["userEmail"])){
         $response['status'] = 5;
+        reportKill($response);
     }
-
-    createUser($user_name, $pwd, $user_apellido, $userCd, $userEmail, $userRol);
+    
+    if( createUser($_POST["user_name"], $_POST["pwd"], $_POST["user_apellido"], $_POST["userCd"], $_POST["userEmail"], $_POST["userRol"]) ){
+        $response['status'] = 20;
+        $response['message'] = 'Registro exitoso.';
+    }
 }
 echo json_encode($response);
 
